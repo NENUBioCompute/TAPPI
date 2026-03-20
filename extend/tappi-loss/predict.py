ab9314935b2cbfee7e49504334d466b51dabc675
kai3171 <149638249+kai3171@users.noreply.github.com>
2026-03-20 11:05:37 +0800

    Update predict.py

--- extend/tappi-loss/predict.py
+++ extend/tappi-loss/predict.py
@@ -28,13 +28,16 @@ def build_input_from_mutations(wt_seq, mutation_list):
 
         seq[pos] = mut_aa
         positions.append(pos)
-    positions.sort()
-    # min_pos = min(positions)
-    # max_pos = max(positions)
 
+    min_pos = min(positions)
+    max_pos = max(positions)
 
+    if max_pos - min_pos > 20:
+        raise ValueError(
+            f"Mutation span too large: {min_pos+1}-{max_pos+1}"
+        )
 
-    region = [(pos, pos + 1) for pos in positions]
+    region = [(min_pos, max_pos + 1)]
 
     mut_seq = "".join(seq)
 
